cea1deaecc9456a6bbc842375b7816a0a442ade4
Alicia Ramos <aliciaramos@icloud.com>
2020-04-17 21:30:15 -0700

    Completed wk2-wed-inClass assignment.

--- CS276/ramos-wk2-Wed-inClass.sql
+++ CS276/ramos-wk2-Wed-inClass.sql
@@ -2,7 +2,7 @@ set serveroutput on;
 set feedback on;
 
 declare
-  v_operator varchar2(1) := '+';
+  v_operator varchar2(1) := '?';
   v_num_1 number(8,2) := 23;
   v_num_2 number(8,2) := 7;
   v_answer number(8,2) := 0;
