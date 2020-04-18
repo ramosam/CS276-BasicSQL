499a15b57dbb09abc119a74e29bfd9b9343e498e
Alicia Ramos <aliciaramos@icloud.com>
2020-04-17 21:21:00 -0700

    Getting started on Wed in class assignment.

---
+++ CS276/ramos-wk2-Wed-inClass.sql
@@ -0,0 +1,28 @@
+set serveroutput on;
+set feedback on;
+
+declare
+  v_operator varchar2(1) := '+';
+  v_num_1 number(8,2) := 23;
+  v_num_2 number(8,2) := 7;
+  v_answer number(8,2) := 0;
+begin
+  case v_operator
+    when '+' then
+      v_answer := v_num_1 + v_num_2;
+    when '-' then
+      v_answer := v_num_1 - v_num_2;
+    when '*' then
+      v_answer := v_num_1 * + v_num_2;
+    when '/' then
+      if v_num_2 = 0 then
+        v_answer := 0;
+      else
+        v_answer := v_num_1 / v_num_2;
+      end if;
+    else
+      v_answer := 0;
+  end case;
+  dbms_output.put_line('The answer to '|| v_num_1 || ' ' || v_operator || ' ' || 
+    v_num_2 || ' is ' || v_answer || '.');
+end;
