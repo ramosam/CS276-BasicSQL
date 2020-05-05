52e1a1ba8caf9280f7fd87ee3575cd9013e8753d
Alicia Ramos <aliciaramos@icloud.com>
2020-05-04 21:25:44 -0700

    testing out different datatype declarations.

--- CS276/ramos-wk5-Mon-inClass.sql
+++ CS276/ramos-wk5-Mon-inClass.sql
@@ -1,30 +1,34 @@
 set serveroutput on;
 
-
+drop table person cascade constraints;
 create table person (
-  person_id pls_integer primary_key,
+  person_id number primary key,
   f_name varchar2(20) not null,
   l_name varchar2(20) not null,
-  m_i_name varchr2(1) not null
+  m_i_name varchar2(1) not null
 );
 
 declare
   type person_rec is record
   (
-    person_id pls_integer, --could also use %type
+    person_id number, --could also use %type
     f_name varchar2(20), --%type
     l_name varchar2(20), --%type
     m_i_name varchar2(1) --%type
   );
   -- implicit record
-  a_person person_rec;
+  a_person person%rowtype; --person_rec
 begin
   -- initalize field values
   a_person.person_id := 100;
-  a_person.f_name := 'Pamela';
-  a_person.l_name := 'Farr';
+  a_person.f_name := 'Teddy';
+  a_person.l_name := 'Bear';
   a_person.m_i_name := 'M';
   -- insert record into table
   insert into person
-    values person_rec;
+    values a_person;
 end;
+
+
+select * from person where l_name = 'Bear';
+commit;
